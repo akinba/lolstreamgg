ba25ff70597cb77f405d3c921f804fe59da30c87
SERKAN AKIN <akinba@gmail.com>
2018-06-19 07:49:54 +0300

    'OK'

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const app	= require("express")();
+const express	= require("express");
+const app		= express();
 app.set("view engine", "ejs");
-
+app.use( express.static("public"));
 
 app.get("/",(req,res)=>{
-	res.send('Haziriiim');
+	res.render('index');
 });
 
 app.listen(3000, process.env.IP, function(){
